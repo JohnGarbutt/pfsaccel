f97b9e341a8c97d1480662095bfb3baa9e2e88e0
John <john.garbutt@stackhpc.com>
2018-05-18 17:05:20 +0100

    Add creation of slice, when buffer created

--- pfsaccel/demo.go
+++ pfsaccel/demo.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"context"
+	"container/list"
 	"github.com/coreos/etcd/clientv3"
 	"github.com/coreos/etcd/clientv3/clientv3util"
 )
@@ -23,28 +24,18 @@ func main() {
 
 	// tidy up keys once we are finished
 	var tidyup = func() {
+		fmt.Println(kvc.Get(context.Background(), "/buffer", clientv3.WithPrefix()))
 		fmt.Println(kvc.Delete(context.Background(), "/buffer", clientv3.WithPrefix()))
+		fmt.Println(kvc.Get(context.Background(), "/slice", clientv3.WithPrefix()))
+		fmt.Println(kvc.Delete(context.Background(), "/slice", clientv3.WithPrefix()))
 	}
 	tidyup()
 	defer tidyup()
 
-	// watch for updates
-	rch := cli.Watch(context.Background(), "/buffer", clientv3.WithPrefix())
-	var watch_buffer = func() {
-		for wresp := range rch {
-			for _, ev := range wresp.Events {
-				fmt.Printf("%s %q : %q\n", ev.Type, ev.Kv.Key, ev.Kv.Value)
-			}
-		}
-	}
-	go watch_buffer()
-
-	// atomic add new key
-	var atomic_add = func(id int) {
-		var  key = fmt.Sprintf("/buffer/%s", id)
+	var atomic_add = func(key string, value string) {
 		response, err := kvc.Txn(context.Background()).
 			If(clientv3util.KeyMissing(key)).
-			Then(clientv3.OpPut(key, "hello mr buffer")).
+			Then(clientv3.OpPut(key, value)).
 			Commit()
 		if err != nil {
 			panic(err)
@@ -54,10 +45,42 @@ func main() {
 		}
 	}
 
+	var atomic_add_buffer = func(id int) {
+		var  key = fmt.Sprintf("/buffer/%d", id)
+		atomic_add(key, "I am a new buffer")
+	}
+
+	// list of "available" slice ids
+	slice_list := list.New()
+	slice_ids := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	for _, id := range slice_ids {
+		slice_list.PushBack(id)
+	}
+	slice_list_next := slice_list.Front()
+
+	var watch_prefix = func(prefix string, onPut func(event *clientv3.Event)) {
+		rch := cli.Watch(context.Background(), prefix, clientv3.WithPrefix())
+		for wresp := range rch {
+			for _, ev := range wresp.Events {
+				if ev.Type.String() == "PUT" {
+					onPut(ev)
+				}
+				fmt.Printf("%s %q : %q\n", ev.Type, ev.Kv.Key, ev.Kv.Value)
+			}
+		}
+	}
+
+	// watch for buffers, create slice on put
+	make_slice := func(event *clientv3.Event) {
+		atomic_add(fmt.Sprintf("/slice/%d", slice_list_next.Value), string(event.Kv.Key))
+		slice_list_next = slice_list_next.Next()
+	}
+	go watch_prefix("/buffer", make_slice)
+
 	// add some fake buffers to test the watch
 	ids := []int{1, 2, 3, 4, 5}
 	for _, id := range ids {
-		go atomic_add(id)
+		atomic_add_buffer(id)
 	}
-	atomic_add(16)
+	atomic_add_buffer(16)
 }
